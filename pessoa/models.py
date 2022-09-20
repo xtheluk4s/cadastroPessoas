862a2fa85f9bd49c29b0f53e561c5721c39ce085
Lucas Araujo Costa <lucasaraujo1964@gmail.com>
2022-09-20 19:23:32 -0300

    App Pessoa criado

---
+++ pessoa/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Pessoa(models.Model):
+    nome_completo = models.CharField(max_length=256)
+    data_nascimento = models.DateField(null=True)
+    ativa = models.BooleanField(default=True)
+    
